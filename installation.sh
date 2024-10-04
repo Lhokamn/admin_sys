5a3c852ac3b3ddf9465ab75301de066c6a58bb2d
Lhokam <corentin@cclaudel.fr>
2024-10-04 11:48:51 +0200

    Modification des conditions d'accès et des variables

--- installation.sh
+++ installation.sh
@@ -11,6 +11,8 @@ userSSHGroup="ssh_users"
 userPasswd=(openssl rand -hex 20)
 cryptUserPasswd=$(perl -e 'print crypt($ARGV[0], "password")' $userPasswd)
 
+osdetected=$true
+
 ## Start Script
 
 echo "Starting script"
@@ -26,7 +28,7 @@ echo "Dectection OS, packages will be adapt"
     os_name="$NAME"
     os_version="$VERSION"
 
-if [[ $system == "Debian*" ]]; then
+if [[ $os_name == "Debian*"  -o $os_name == "Ubuntu*" ]]; then
 
     echo "Debian OS detected, start installation application"
 
@@ -36,32 +38,36 @@ if [[ $system == "Debian*" ]]; then
     # Install openssh for server
     apt install openssh-server fail2ban ufw -y > /dev/null
 
-elif [[ $system == "" ]]; then
+elif [[ $os_name == "Fedora*" ]]; then
 
 else 
     echo "unrecognize system"
+    $osdetected=$false
 
 fi
 
-echo "Everything is up to date"
+if [[ $osdetected ]]
 
-echo "configuration sshd"
-# config ssh connexion
-cp ssh/sshd_config /etc/ssh/sshd_config
-echo "AllowGroups $userSSHGroup" >> /etc/ssh/sshd_config
-systemctl restart sshd
+    echo "configuration sshd"
+    # config ssh connexion
+    echo "AllowGroups $userSSHGroup" >> /etc/ssh/sshd_config
+    systemctl restart sshd > /dev/null
 
-echo "Firewall configuratio"
-# Open ssh port with tcp
-ufw allow ssh/tcp
-ufw enable
+    echo "Firewall configuratio"
+    # Open ssh port with tcp
+    ufw allow ssh/tcp > /dev/null
+    ufw enable > /dev/null
 
-echo "Disablong IPv6"
-# Disable IPv6
-echo "# Disabling the IPv6" >> /etc/sysctl.conf
-echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
-echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
-echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
-update-initramfs -u
+    echo "Disabling IPv6"
+    # Disable IPv6
+    echo "# Disabling the IPv6" >> /etc/sysctl.conf
+    echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+    echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+    echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
+    update-initramfs -u > /dev/null
 
-echo "You server is Ready"
+    echo "You server is Ready"
+else
+    echo "Problem at configuration"
+
+fi
