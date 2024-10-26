b0e4da6b6da4dbffccc211cc9b60ce4c0d845cd7
Lhokam <corentin@cclaudel.fr>
2024-10-26 18:37:59 +0200

    Rajout de la configuration du script

---
+++ configurations/README.md
@@ -0,0 +1,6 @@
+# Sommaire configuration
+
+- [dhcp](https://doc.cclaudel.fr/configuration/dhcp)
+- [dns](https://doc.cclaudel.fr/configuration/dns)
+- [linux comme frontend](https://doc.cclaudel.fr/configuration/frontend)
+- [ssh](https://doc.cclaudel.fr/configuration/ssh)
--- installation.sh
+++ installation.sh
@@ -2,74 +2,76 @@
 
 echo "Start preparation"
 
-#System detection for command
-system=$OSTYPE
+# Variable modifiable
+user="admcld"
+userSSHGroup="ssh_users"
 
-if [[ $system == "debian" ]]; then
+## Start Script
 
-    echo "Utilisateur ssh"
-    read userssh
+echo "Starting script"
+
+echo "creation of ssh user : $user"
+groupadd $userSSHGroup
+userPasswd=(openssl rand -hex 20)
+cryptUserPasswd=$(perl -e 'print crypt($ARGV[0], "password")' $userPasswd)
+useradd -m -G $userSSHGroup -p $cryptUserPasswd 
+
+
+echo "Dectection OS, packages will be adapt"
+
+# By default, os_detected is at true because he don't have to change it at any os, we change it only if os was not detected
+osdetected=$true
+
+. /etc/os-release
+    os_name="$NAME"
+    os_version="$VERSION"
+
+if [[ $os_name == "Debian*"  -o $os_name == "Ubuntu*" ]]; then
+
+    echo "Debian OS detected, start installation application"
 
     # Update Packages
-    apt update & apt upgrade -y
-
-    # Network configuration
-    echo "Adresse IPv4"
-    read IPv4
-
-    echo "Entrez masque sous réseaux"
-    read mask
-
-    echo "Passerelle par défaut"
-    read gateway
-
-    echo "Interface"
-    read networkInterface
-
-    echo "auto $networkInterface \n
-    iface $networkInterface inet static \n
-    address $IPv4 \n
-    netmask $mask \n
-    gateway $gateway" >> /etc/network/interfaces
-
-    systemctl restart networking
-
+    apt update & apt upgrade -y > /dev/null
 
     # Install openssh for server
-    apt install openssh-server -y
+    apt install openssh-server fail2ban ufw -y > /dev/null
 
+elif [[ $os_name == "Fedora*" ]]; then
+
+else 
+    echo "unrecognize system"
+    $osdetected=$false
+
+fi
+
+if [[ $osdetected ]]
+
+    echo "configuration sshd"
     # config ssh connexion
-    cp ssh/sshd_config /etc/ssh/sshd_config
-    echo "AllowUsers $userssh" >> /etc/ssh/sshd_config
-    systemctl restart ssh
+    # awk '{gsub(/PermitRootLogin yes/, "PermitRootLogin no"); print}' /etc/ssh/sshd_config > /etc/ssh/sshd_config  # Remplacement de la valeur
+    # awk '{gsub(/#MaxAuthTries 6/, "MaxAuthTries 3"); print}' /etc/ssh/sshd_config > /etc/ssh/sshd_config          # Remplacement de la valeur
+    # awk '{gsub(/#MaxSessions 10/, "MaxSessions 3"); print}' /etc/ssh/sshd_config > /etc/ssh/sshd_config           # Remplacement de la valeur
+    echo "AllowGroups $userSSHGroup" >> /etc/ssh/sshd_config
+    systemctl restart sshd > /dev/null
 
-    # Install fail2ban
-    apt install fail2ban -y
-
-    apt install rsyslog -y 
-    cp ssh/jail.local /etc/fail2ban/jail.local
-    systemctl restart fail2ban
-
-    # Install ufw firewall
-    apt install ufw -y
-
+    echo "Firewall configuration"
     # Open ssh port with tcp
-    ufw allow ssh/tcp
-    ufw enable
+    ufw allow ssh/tcp > /dev/null
+    ufw enable > /dev/null
 
+    echo "fail2ban configuration"
+    wget -P /etc/fail2ban/ https://doc.cclaudel.fr/configurations/ssh/jail.local
+
+    echo "Disabling IPv6"
     # Disable IPv6
-    # Désactiver IPv6
     echo "# Disabling the IPv6" >> /etc/sysctl.conf
     echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
     echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
     echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
+    update-initramfs -u > /dev/null
 
-    update-initramfs -u
-
-
-    echo "Everything is update !"
-else 
-    echo "unrecognize system"
+    echo "You server is Ready"
+else
+    echo "Problem at configuration"
 
 fi
-
